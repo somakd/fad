3789badd845d619e340c53f67f3c61a9fc7d958b
fanstats <31288556+fanstats@users.noreply.github.com>
2021-05-26 14:39:54 -0400

    Update fad.r

--- R/fad.r
+++ R/fad.r
@@ -42,11 +42,12 @@
 #' @param \dots Components of \code{control} can also be supplied as named arguments to \code{fad}.
 #'
 #' @return An object of class \code{"fad"} with components
-#'\item{loadings}{A matrix of loadings, one column for each factor.  The
+#'\item{loadings}{A matrix of loadings on the correlation scale, one column for each factor.  The
 #' factors are ordered in decreasing order of sums of squares of
 #' loadings, and given the sign that will make the sum of the loadings
 #' positive.  This is of class \code{"loadings"}}
-#' \item{uniquenesses}{The uniquenesses computed.}
+#' \item{uniquenesses}{The uniquenesses computed on the correlation scale.}
+#' \item{sd}{The estimated standard deviations.}
 #' \item{criteria}{The results of the optimization: the value of the
 #' criterion (a linear function of the negative log-likelihood) and information
 #' on the iterations used.}
